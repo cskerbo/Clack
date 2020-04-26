1ec6d79002e9d0924e95df630ea53f0fdfda5e02
cskerbo <54558554+cskerbo@users.noreply.github.com>
2020-04-26 16:36:18 -0700

    Added attribute normalizer to prevent white space after user email input

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_one_attached :avatar
   validates :email, :username, presence: true, uniqueness: true
   validates :password, presence: true, length: {minimum: 6}
+  normalize_attribute :email, :with => :strip
 end
